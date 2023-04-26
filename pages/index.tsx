e575da4c561167c5017422efef4ab157d17abb2d
VitorCosta <Costa-Vitor-Fernandes>
2023-04-25 22:46:39 -0300

    a happy msg

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,7 +8,8 @@ export default function Home() {
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
     >
-      <div className='bg-red-500'>THE SIZE PICKER</div>
+      <div className='bg-red-500'>THE SIZE PICKER</div> 
+      <div className='bg-green-500 mt-2'> This one Works !</div>
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           Get started by editing&nbsp;
