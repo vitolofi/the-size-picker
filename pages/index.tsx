90c21c4b00c74f205eccb609e2f908d35c745dc2
VitorCosta <Costa-Vitor-Fernandes>
2023-05-30 21:33:46 -0300

    we have a project. no bs. just ugly as f*

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,13 +3,13 @@ import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
 
-export default function Home() {
+export default function Home() { 
   return (
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
     >
       <div className='bg-red-500'>THE SIZE PICKER</div> 
-      <div className='bg-green-500 mt-2'> This one Works !</div>
+      <div className='bg-green-500 mt-2'> This one Works ! with size factory working</div>
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           Get started by editing&nbsp;
