6d9a27e2f17fe8a837de7db73bec7c7982dc1de8
VitorCosta <Costa-Vitor-Fernandes>
2023-06-13 17:48:38 -0300

    removes description

--- components/sugar/SugarBusto.tsx
+++ components/sugar/SugarBusto.tsx
@@ -12,6 +12,6 @@ export default function SugarBustoSVG(props:{bustoColor:string, description:stri
 <svg className='relative top-40 left-20' width="140" height="25" viewBox="0 0 273 25" fill="none" xmlns="http://www.w3.org/2000/svg" >
 <path className={`stroke${props.bustoColor} shadow-lg outline-1 opacity-25`} d="M1 14L59 20H129L210.198 15.0904C227.703 14.0319 245.092 11.5362 262.188 7.62847L271.5 5.5" strokeWidth="10"/>
 </svg>
-        <div className={`relative inline-block top-40 left-28 p-1 bg${props.bustoColor} text-black text-center`}>Busto:{props.description}</div>
+        {/* <div className={`relative inline-block top-40 left-28 p-1 bg${props.bustoColor} text-black text-center`}>Busto:{props.description}</div> */}
     </div>)
 }
--- components/sugar/SugarQuadril.tsx
+++ components/sugar/SugarQuadril.tsx
@@ -11,6 +11,6 @@ export default function SugarQuadrilSVG(props:{quadrilColor:string,description:s
 <svg className='relative top-44 left-16' width="155" height="25" viewBox="0 0 273 25" fill="none" xmlns="http://www.w3.org/2000/svg">
 <path className={`stroke${props.quadrilColor} shadow-lg outline-1 opacity-25`}d="M1 14L59 20H129L210.198 15.0904C227.703 14.0319 245.092 11.5362 262.188 7.62847L271.5 5.5" strokeWidth="10"/>
 </svg>
-<div className={`relative top-[12.225rem] left-28 inline-block p-1 bg${props.quadrilColor} text-black`}>Quadril:{props.description}</div>
+{/* <div className={`relative top-[12.225rem] left-28 inline-block p-1 bg${props.quadrilColor} text-black`}>Quadril:{props.description}</div> */}
     </div>)
 }
