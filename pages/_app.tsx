4a8cf24f1fb85a6adc783dbeca0c9884832a7e1d
VitorCosta <Costa-Vitor-Fernandes>
2023-06-13 17:50:21 -0300

    adds google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,21 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import Script from 'next/script'
+import { Head } from 'next/document'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <>
+
+  <Script async src="https://www.googletagmanager.com/gtag/js?id=G-4R1WETXQ5E"></Script>
+<Script>{`
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'G-4R1WETXQ5E', {page_path:window.location.pathname});
+  `
+  }
+</Script>
+  <Component {...pageProps} />
+  </>
 
 }
