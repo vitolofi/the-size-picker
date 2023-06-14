31b94844cd84e3ba45293bf313c074006a78f958
VitorCosta <Costa-Vitor-Fernandes>
2023-06-14 11:48:34 -0300

    check icon feature

--- components/ChangeSizeButtons.tsx
+++ components/ChangeSizeButtons.tsx
@@ -46,14 +46,26 @@ return(<div className="flex my-4">
     {allSizesNames.map((v,i,arr)=>{
       if(selectedSize===v){
         if(props.preferedSize === v){
-        return <button key={i} className="rounded-lg bg-black text-white shadow-lg py-2 mx-2 px-2">{v}!</button>
+        return <><div>
+            <div className="absolute h-[1rem] ">
+            <img className="bg-white rounded-full relative  h-[1rem]" src="/1008958.png"/>
+            </div>
+            </div>
+        <button key={i} className="rounded-lg bg-black text-white shadow-lg py-2 mx-2 px-2">{v}</button>
+        </>
         // ! é o recomendado 
             }
         else
         return <button key={i} className="rounded-lg bg-black text-white shadow-lg py-2 mx-2 px-2">{v}</button>
       }
          else if(props.preferedSize === v){
-            return <button key={i} className="rounded-lg bg-white shadow-lg py-2 mx-2 text-black px-2" onClick={()=>changeSize(i)}>{v}!</button>
+            return <><div>
+                <div className="absolute h-[1rem]">
+                <img className="bg-white rounded-full relative  h-[1rem]" src="/1008958.png"/>
+                </div>
+                </div>
+            <button key={i} className="rounded-lg bg-white shadow-lg py-2 mx-2 text-black px-2" onClick={()=>changeSize(i)}>{v}</button>
+            </>
          }
          
          else return <button key={i} className="rounded-lg bg-white shadow-lg py-2 mx-2 text-black px-2" onClick={()=>changeSize(i)}>{v}</button>
